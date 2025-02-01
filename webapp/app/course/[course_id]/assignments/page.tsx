febf258ac8adcd8051c6984307c47717a5cb2903
Jonathan Bell <jon@jonbell.net>
2025-02-01 10:11:17 -0500

    fix broken build

---
+++ webapp/app/course/[course_id]/assignments/page.tsx
@@ -0,0 +1,7 @@
+export default async function AssignmentsPage({ params }: { params: Promise<{ courseId: string }>}) {
+    return (
+        <div>
+            <h1>Assignments</h1>
+        </div>
+    );
+}
