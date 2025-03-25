59edf2c149a94fee45ca7c250fcd4f8fd39a6aa5
Jonathan Bell <jon@jonbell.net>
2025-03-25 13:37:16 -0400

    :facepalm:

---
+++ webapp/app/error/page.tsx
@@ -0,0 +1,20 @@
+import { VStack, Container, Stack, Heading, Text } from "@chakra-ui/react";
+
+export default async function ErrorPage({ searchParams }: { searchParams: Promise<{ error: string, error_code: string, error_description: string }> }) {
+    const { error, error_code, error_description } = await searchParams;
+    return (
+        <Container maxW="md" py={{ base: '12', md: '24' }}>
+            <Stack gap="6">
+                <VStack gap="2" textAlign="center" mt="4">
+                    <Heading size="3xl">Error</Heading>
+                    <Text color="fg.muted">
+                        {error_description}
+                    </Text>
+                    <Text color="fg.muted">
+                        Try using your browser's back button to go back to the previous page, and **TODO** file a bug report.
+                    </Text>
+                </VStack>
+            </Stack>
+        </Container>
+    )
+}
