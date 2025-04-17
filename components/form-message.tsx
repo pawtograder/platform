cab7af6aea4acf96bd3e27a3d72237766b2e89f3
Jonathan Bell <jon@jonbell.net>
2025-04-17 09:12:17 -0400

    Add reset password flow

--- components/form-message.tsx
+++ components/form-message.tsx
@@ -1,24 +1,33 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 export type Message =
   | { success: string }
-  | { error: string }
+  | { error: string; error_code?: string; error_description?: string }
   | { message: string };
 
 export function FormMessage({ message }: { message: Message }) {
   return (
-    <div className="flex flex-col gap-2 w-full max-w-md text-sm">
+    <Flex flexDirection="column" gap="2" w="full" maxW="md" textStyle="sm">
       {"success" in message && (
-        <div className="text-foreground border-l-2 border-foreground px-4">
-          {message.success}
-        </div>
+        <Box borderLeft="4px solid" borderColor="border.success" pl="4" bg="bg.success"
+        mb="4">
+          <Text colorPalette="green">{message.success}</Text>
+        </Box>
       )}
       {"error" in message && (
-          <Text color="red">{message.error}</Text>
+        <Box borderLeft="4px solid" borderColor="border.error" pl="4" bg="bg.error"
+        mb="4">
+          <Text fontWeight="bold" colorPalette="red">Error: {message.error}</Text>
+          {message.error_code && <Text fontWeight="bold" colorPalette="red">Error code: {message.error_code}</Text>}
+          {message.error_description && <Text colorPalette="red">{message.error_description}</Text>}
+        </Box>
       )}
       {"message" in message && (
-        <div className="text-foreground border-l-2 px-4">{message.message}</div>
+        <Box borderLeft="4px solid" borderColor="border.gray" pl="4" bg="bg.gray"
+        mb="4">
+          <Text colorPalette="gray">{message.message}</Text>
+        </Box>
       )}
-    </div>
+    </Flex>
   );
 }
