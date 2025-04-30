5ea094bff89df46a0ed83de8be873b276cd4191e
Jonathan Bell <jon@jonbell.net>
2025-04-30 14:44:24 -0400

    Support course-specific canvas keys

--- supabase/functions/_shared/CanvasWrapper.ts
+++ supabase/functions/_shared/CanvasWrapper.ts
@@ -1,9 +1,14 @@
 import { CanvasApi } from "npm:@kth/canvas-api";
 import { Course, Enrollment, UserProfile } from "../_shared/CanvasTypes.d.ts";
 
-const canvas = new CanvasApi(Deno.env.get("CANVAS_API_URL")!, Deno.env.get("CANVAS_API_KEY")!);
 
-export async function getEnrollments({ canvas_course_id, canvas_course_section_id }: { canvas_course_id: number | null, canvas_course_section_id: number | null }): Promise<Enrollment[]> {
+function getCanvas(id: number) {
+    const canvas_api_url = Deno.env.get(`CANVAS_API_URL_${id}`) || Deno.env.get("CANVAS_API_URL");
+    const canvas_api_key = Deno.env.get(`CANVAS_API_KEY_${id}`) || Deno.env.get("CANVAS_API_KEY");
+    return new CanvasApi(canvas_api_url!, canvas_api_key!);
+}
+export async function getEnrollments({ class_id, canvas_course_id, canvas_course_section_id }: { class_id: number, canvas_course_id: number | null, canvas_course_section_id: number | null }): Promise<Enrollment[]> {
+    const canvas = getCanvas(class_id);
     if (canvas_course_id) {
         console.log("Getting enrollments for course", canvas_course_id);
         const pages = await canvas.listPages(`courses/${canvas_course_id}/enrollments`);
@@ -24,19 +29,13 @@ export async function getEnrollments({ canvas_course_id, canvas_course_section_i
     }
     throw new Error("Either canvas_course_id or canvas_section_id must be provided");
 }
-export async function getUser(userId: number): Promise<UserProfile> {
+export async function getUser(classId: number, userId: number): Promise<UserProfile> {
+    const canvas = getCanvas(classId);
     const { json } = await canvas.get(`users/${userId}/profile`);
     return json;
 }
 export async function getCourse(courseId: number): Promise<Course> {
+    const canvas = getCanvas(courseId);
     const { json } = await canvas.get(`courses/${courseId}`);
     return json;
-}
-export async function listCourses(): Promise<Course[]> {
-    const pages = await canvas.listPages("courses");
-    const ret = [];
-    for await (const page of pages) {
-        ret.push(...page.json);
-    }
-    return ret;
 }
