1ff8d4f4ffd67e6d2c38f5bc45c09f71cb669168
Aritra (Ari) Saharay <80832201+Ari-S-123@users.noreply.github.com>
2025-05-05 12:21:31 -0700

    #38 - fix 😮‍💨

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
+
+const eslintConfig = [...compat.config({ extends: ["next", "prettier"] }), { rules: { "no-console": "warn" } }];
+
+export default eslintConfig;
