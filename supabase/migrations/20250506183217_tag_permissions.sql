82f1cb520eb30a37ef0806e3de74bbc93d6bf482
Lauren Brissette <168485735+laurenbrissette@users.noreply.github.com>
2025-05-12 17:02:56 -0400

    Instructors can view all tags + userid to profileid + add foreign key references

--- supabase/migrations/20250506183217_tag_permissions.sql
+++ supabase/migrations/20250506183217_tag_permissions.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS "public"."tags" (
-    "id" uuid NOT NULL,
+    "id" uuid NOT NULL PRIMARY KEY,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
     "name" "text" NOT NULL,
     "color" "text" NOT NULL,
     "visible" boolean NOT NULL,
-    "user_id" uuid NOT NULL, 
-    "class_id" bigint NOT NULL
+    "profile_id" uuid NOT NULL REFERENCES "public"."profiles"(id), 
+    "class_id" bigint NOT NULL REFERENCES "public"."classes"(id)
 );
 
 alter table "public"."tags" enable row level security;
@@ -20,12 +20,21 @@ using (
     OR authorizeforclassinstructor(class_id)
 );
 
+CREATE POLICY "graders and instructors can view all class tags"
+ON "public"."tags"
+AS PERMISSIVE
+FOR SELECT
+USING (
+    authorizeforclassgrader(class_id)
+    OR authorizeforclassinstructor(class_id)
+);
+
 CREATE POLICY "users can view their visible tags for class"
 ON "public"."tags"
 AS PERMISSIVE
 FOR SELECT
 USING (
     visible = true AND 
-    user_id = auth.uid() AND
+    authorizeforprofile("profile_id") AND
     authorizeforclass("class_id") 
 );
