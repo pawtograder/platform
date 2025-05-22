f5d2436aa1aac7fa2c8974a0ce7bc499f762a2b5
Jonathan Bell <jon@jonbell.net>
2025-05-22 13:38:13 -0400

    Update migration to patch existing rubrics

--- supabase/migrations/20250519202920_rubrics_column_change_nullable.sql
+++ supabase/migrations/20250519202920_rubrics_column_change_nullable.sql
@@ -1,3 +1,5 @@
+UPDATE public.rubrics set assignment_id=assignments.id FROM assignments where assignments.grading_rubric_id=rubrics.id;
+
 alter table "public"."rubrics" alter column "assignment_id" set not null;
 
 
