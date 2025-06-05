76ccc19f1a5e3fe0d70acfd628ee8692de7739bc
Aritra (Ari) Saharay <80832201+Ari-S-123@users.noreply.github.com>
2025-06-04 19:21:04 -0700

    #81 - Deleted unused file

--- app/course/[course_id]/manage/assignments/new/submit.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const data = req.body;
-  // eslint-disable-next-line no-console
-  const id = console.log(data);
-  res.status(200).json({ id });
-}
