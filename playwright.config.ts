9bbabcc5cd30b6293af443ee75dbe2ebd688be1e
Jonathan Bell <jon@jonbell.net>
2025-07-08 21:05:57 -0400

    Reduce concurrency for e2e tests in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: 0, //process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 2, //process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 2,
 
   timeout: 20_000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
