74f3254392022973bcef22f7ebd8163b52913021
Jonathan Bell <jon@jonbell.net>
2025-08-18 13:20:35 -0400

    More race reduction

--- tests/e2e/grading.test.tsx
+++ tests/e2e/grading.test.tsx
@@ -183,7 +183,9 @@ test.describe("An end-to-end grading workflow self-review to grading", () => {
 
     // Release All Submission Reviews
     await page.goto(`/course/${course.id}/manage/assignments/${assignment!.id}`);
+    await page.waitForLoadState();
     await page.getByRole("button", { name: "Release All Submission Reviews", exact: true }).click();
+    await page.waitForLoadState();
     await page.goto(`/course/${course.id}/assignments/${assignment!.id}/submissions/${submission_id}`);
     await expect(page.getByText("Released to studentYes")).toBeVisible();
   });
