9a109409b97e99abb1f5ed0edacb5d1294ff3ce0
Aritra (Ari) Saharay <80832201+Ari-S-123@users.noreply.github.com>
2025-08-26 08:55:00 -0700

    #127 - tweak

--- app/course/[course_id]/manage/course/due-date-extensions/tables/studentExtensionsTable.tsx
+++ app/course/[course_id]/manage/course/due-date-extensions/tables/studentExtensionsTable.tsx
@@ -21,8 +21,6 @@ export default function StudentExtensionsTable() {
   const students = useAllStudentProfiles();
   const supabase = createClient();
 
-  console.log("extensions", extensions);
-
   const createOpen = useModalManager<AddExtensionDefaults>();
   const editOpen = useModalManager<StudentDeadlineExtension>();
 
