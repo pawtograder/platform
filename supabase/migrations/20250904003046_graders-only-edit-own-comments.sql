9c5da781172d1e2a30cc03101ead01081b962b06
Jonathan Bell <jon@jonbell.net>
2025-09-03 21:07:13 -0400

    better rls

--- supabase/migrations/20250904003046_graders-only-edit-own-comments.sql
+++ supabase/migrations/20250904003046_graders-only-edit-own-comments.sql
@@ -7,6 +7,7 @@ drop policy if exists "Only graders and instructors can update" on "public"."sub
 drop policy if exists "Only graders and instructors can update" on "public"."submission_artifact_comments";
 drop policy if exists "Update self only" on "public"."submission_file_comments";
 
+
 -- Create new update policies that allow:
 -- 1. Instructors to update any comment in their class
 -- 2. Graders to only update comments they authored
@@ -16,18 +17,21 @@ on "public"."submission_comments"
 as permissive
 for update
 to public
-using (authorizeforclassinstructor(class_id) OR (authorizeforclassgrader(class_id) AND authorizeforprofile(author)));
+using (authorizeforclassinstructor(class_id) OR (authorizeforclassgrader(class_id) AND authorizeforprofile(author)))
+with check (authorizeforclassinstructor(class_id) OR (authorizeforclassgrader(class_id) AND authorizeforprofile(author)));
 
 create policy "Instructors can update all, graders only own comments"
 on "public"."submission_artifact_comments"
 as permissive
 for update
 to public
-using (authorizeforclassinstructor(class_id) OR (authorizeforclassgrader(class_id) AND authorizeforprofile(author)));
+using (authorizeforclassinstructor(class_id) OR (authorizeforclassgrader(class_id) AND authorizeforprofile(author)))
+with check (authorizeforclassinstructor(class_id) OR (authorizeforclassgrader(class_id) AND authorizeforprofile(author)));
 
 create policy "Instructors can update all, graders only own comments"
 on "public"."submission_file_comments"
 as permissive
 for update
 to public
-using (authorizeforclassinstructor(class_id) OR (authorizeforclassgrader(class_id) AND authorizeforprofile(author)));
+using (authorizeforclassinstructor(class_id) OR (authorizeforclassgrader(class_id) AND authorizeforprofile(author)))
+with check (authorizeforclassinstructor(class_id) OR (authorizeforclassgrader(class_id) AND authorizeforprofile(author)));
