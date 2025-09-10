2df23b303a4b7399ce98e7209ede8c88e53ccd19
Jonathan Bell <jon@jonbell.net>
2025-09-10 13:26:35 -0400

    disable what if

--- app/course/[course_id]/gradebook/page.tsx
+++ app/course/[course_id]/gradebook/page.tsx
@@ -3,13 +3,16 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 import WhatIf from "./whatIf";
 
 export default function GradebookPage() {
+  /*
+  To use the &quot;What If&quot; grade
+        simulator, click on a score for an assignment, and edit the value. Calculated fields will automatically
+        re-calculate, and you can not edit those fields directly.
+  */
   return (
     <Box p={4}>
       <Heading size="lg">Gradebook</Heading>
       <Text fontSize="sm" color="fg.muted">
-        Grades that have been released by your instructor are shown below. To use the &quot;What If&quot; grade
-        simulator, click on a score for an assignment, and edit the value. Calculated fields will automatically
-        re-calculate, and you can not edit those fields directly.
+        Grades that have been released by your instructor are shown below.
       </Text>
       <WhatIf />
     </Box>
