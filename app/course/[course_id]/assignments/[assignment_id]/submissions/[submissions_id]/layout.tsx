8c2fbb027444aeaf1a2eec7a5518106638ed1642
Jonathan Bell <jon@jonbell.net>
2025-09-24 08:37:24 -0400

    Remove complete submission review button when a user has individual rubric parts assigned for that submission. Add a release/unrelease button back to each submission, only for instructors

--- app/course/[course_id]/assignments/[assignment_id]/submissions/[submissions_id]/layout.tsx
+++ app/course/[course_id]/assignments/[assignment_id]/submissions/[submissions_id]/layout.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/utils/supabase/DatabaseTypes";
 import { Box, Flex, Heading, HStack, List, Skeleton, Table, Text, VStack } from "@chakra-ui/react";
 
+import { AdjustDueDateDialog } from "@/app/course/[course_id]/manage/assignments/[assignment_id]/due-date-exceptions/page";
 import { ActiveSubmissionIcon } from "@/components/ui/active-submission-icon";
 import AskForHelpButton from "@/components/ui/ask-for-help-button";
 import { DataListItem, DataListRoot } from "@/components/ui/data-list";
@@ -16,8 +17,8 @@ import PersonName from "@/components/ui/person-name";
 import { ListOfRubricsInSidebar, RubricCheckComment } from "@/components/ui/rubric-sidebar";
 import SubmissionReviewToolbar, { CompleteReviewButton } from "@/components/ui/submission-review-toolbar";
 import { toaster, Toaster } from "@/components/ui/toaster";
-import { useAssignmentController } from "@/hooks/useAssignment";
-import { useClassProfiles, useIsGraderOrInstructor, useIsInstructor } from "@/hooks/useClassProfiles";
+import { useAssignmentController, useReviewAssignmentRubricParts } from "@/hooks/useAssignment";
+import { useIsGraderOrInstructor, useIsInstructor } from "@/hooks/useClassProfiles";
 import { useAssignmentDueDate, useCourse } from "@/hooks/useCourseController";
 import {
   SubmissionProvider,
@@ -26,6 +27,7 @@ import {
   useSubmission,
   useSubmissionComments,
   useSubmissionController,
+  useSubmissionReview,
   useSubmissionReviewOrGradingReview
 } from "@/hooks/useSubmission";
 import { useActiveReviewAssignmentId } from "@/hooks/useSubmissionReview";
@@ -61,7 +63,7 @@ import { PiSignOut } from "react-icons/pi";
 import { RxQuestionMarkCircled } from "react-icons/rx";
 import { TbMathFunction } from "react-icons/tb";
 import { linkToSubPage } from "./utils";
-import { AdjustDueDateDialog } from "@/app/course/[course_id]/manage/assignments/[assignment_id]/due-date-exceptions/page";
+import { Alert } from "@/components/ui/alert";
 
 // Create a mapping of icon names to their components
 const iconMap: { [key: string]: ReactElementType } = {
@@ -527,12 +529,23 @@ function ReviewStats() {
   const mostRecentGrader = allRubricInstances.find(
     (instance) => Date.parse(instance.created_at) === mostRecentCreationOrEdit
   )?.author;
+  const isInstructor = useIsInstructor();
   if (!review) {
     return <Skeleton height="20px" />;
   }
   return (
     <DataListRoot orientation="horizontal">
-      <DataListItem label="Released to student" value={review.released ? "Yes" : "No"} />
+      {isInstructor && (
+        <DataListItem
+          label="Released to student"
+          value={
+            <HStack>
+              {review.released ? "Yes" : "No"} <ReleaseOrUnreleaseReviewButton submissionReviewId={review.id} />
+            </HStack>
+          }
+        />
+      )}
+      {!isInstructor && <DataListItem label="Released to student" value={review.released ? "Yes" : "No"} />}
       {completed_by && <DataListItem label="Completed by" value={<PersonName size="2xs" uid={completed_by} />} />}
       {completed_at && <DataListItem label="Completed at" value={formatRelative(completed_at, new Date())} />}
       {checked_by && <DataListItem label="Checked by" value={<PersonName size="2xs" uid={checked_by} />} />}
@@ -551,7 +564,44 @@ function ReviewStats() {
     </DataListRoot>
   );
 }
-
+function ReleaseOrUnreleaseReviewButton({ submissionReviewId }: { submissionReviewId: number }) {
+  const review = useSubmissionReview(submissionReviewId);
+  const submissionController = useSubmissionController();
+  const [updatingReview, setUpdatingReview] = useState(false);
+  if (review?.released) {
+    return (
+      <Button
+        size="xs"
+        variant="outline"
+        colorPalette="red"
+        loading={updatingReview}
+        onClick={async () => {
+          setUpdatingReview(true);
+          await submissionController.submission_reviews.update(submissionReviewId, { released: false });
+          setUpdatingReview(false);
+        }}
+      >
+        Unrelease
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        size="xs"
+        variant="outline"
+        colorPalette="green"
+        loading={updatingReview}
+        onClick={async () => {
+          setUpdatingReview(true);
+          await submissionController.submission_reviews.update(submissionReviewId, { released: true });
+          setUpdatingReview(false);
+        }}
+      >
+        Release
+      </Button>
+    );
+  }
+}
 function ReviewActions() {
   const submission = useSubmission();
   const reviewId = submission.grading_review_id;
@@ -559,10 +609,11 @@ function ReviewActions() {
     throw new Error("No grading review ID found");
   }
   const review = useSubmissionReviewOrGradingReview(reviewId);
-  const { private_profile_id } = useClassProfiles();
-  const submissionController = useSubmissionController();
-  const [updatingReview, setUpdatingReview] = useState(false);
-  const isInstructor = useIsGraderOrInstructor();
+
+  const activeReviewAssignmentId = useActiveReviewAssignmentId();
+  const assignedRubricParts = useReviewAssignmentRubricParts(activeReviewAssignmentId);
+  const isInstructorOrGrader = useIsGraderOrInstructor();
+  const showCompleteReviewButton = assignedRubricParts.length == 0 && isInstructorOrGrader;
   if (!review) {
     return <Skeleton height="20px" />;
   }
@@ -570,12 +621,12 @@ function ReviewActions() {
     <VStack>
       <Toaster />
       <ReviewStats />
-      {isInstructor && (!review.completed_at || (review.completed_at && !review.checked_at)) && (
+      {showCompleteReviewButton && !review.completed_at && (
         <VStack>
           <Heading size="md">Submission Review Actions</Heading>
           <HStack w="100%" justify="space-between">
             {!review.completed_at && <CompleteReviewButton />}
-            {review.completed_at && !review.checked_at && private_profile_id !== review.completed_by && (
+            {/* {review.completed_at && !review.checked_at && private_profile_id !== review.completed_by && (
               <Button
                 variant="surface"
                 loading={updatingReview}
@@ -593,7 +644,7 @@ function ReviewActions() {
               >
                 Mark as Checked
               </Button>
-            )}
+            )} */}
           </HStack>
         </VStack>
       )}
@@ -782,9 +833,10 @@ function SubmissionsLayout({ children }: { children: React.ReactNode }) {
           />
           {Boolean(hasExtension) && ` (${hoursExtended}-hour extension applied)`}
           {canStillSubmit && (
-            <Text fontSize="xs" color="fg.warning">
-              The student can still make a new submission, grading checks will not transfer.
-            </Text>
+            <Alert status="warning">
+              The student can still make a new submission, grading checks will not transfer. Only begin grading this
+              submission if you are certain that the student will not make new submissions.
+            </Alert>
           )}
         </Box>
       )}
