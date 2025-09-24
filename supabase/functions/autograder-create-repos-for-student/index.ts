e8b007e928cc0472e97f0940066685ad1fc01341
Jonathan Bell <jon@jonbell.net>
2025-09-24 19:35:10 -0400

    remove extra filter

--- supabase/functions/autograder-create-repos-for-student/index.ts
+++ supabase/functions/autograder-create-repos-for-student/index.ts
@@ -262,7 +262,6 @@ async function handleRequest(req: Request, scope: Sentry.Scope) {
     .eq("classes.user_roles.user_id", userId)
     .not("template_repo", "is", "null")
     .not("template_repo", "eq", "")
-    .lte("release_date", new Date().toISOString())
     .limit(1000);
   if (assignmentsError) {
     console.error(assignmentsError);
