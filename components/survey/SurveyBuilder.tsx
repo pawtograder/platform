5fae39d9aaf5d861640792da87cbcbebae4059a6
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2025-11-09 13:03:28 -0500

    non-ui wires extended

--- components/survey/SurveyBuilder.tsx
+++ components/survey/SurveyBuilder.tsx
@@ -71,7 +71,81 @@ type Props = {
 
 
 
-const SurveyBuilder = () => {
+const SurveyBuilder = ({ value, onChange }: Props) => {
+  const [survey, setSurvey] = useState<BuilderSurvey>(() => {
+    const parsed = fromJSON(value || "");
+    return parsed ?? makeEmptySurvey();
+  });
+  const [pageIdx, setPageIdx] = useState(0);
+  useEffect(() => {
+    const parsed = fromJSON(value || "");
+    setSurvey(parsed ?? makeEmptySurvey());
+    setPageIdx(0);
+  }, [value]);
+
+  useEffect(() => {
+    onChange(toJSON(survey));
+  }, [survey]);
+
+  const currentPage = survey.pages[pageIdx];
+function updateElement(id: string, patch: Partial<BuilderElement>) {
+  const pages = [...survey.pages];
+  const page = { ...pages[pageIdx] };
+
+  page.elements = page.elements.map((el) => {
+    if (el.id !== id) return el;
+    if ("inputType" in patch && !isText(el)) {
+      return el; 
+    }
+
+    return { ...el, ...patch } as BuilderElement;
+  });
+
+  pages[pageIdx] = page;
+  setSurvey((s) => ({ ...s, pages }));
+}
+
+
+function addChoice(id: string) {
+  const pages = [...survey.pages];
+  const page = { ...pages[pageIdx] };
+  page.elements = page.elements.map((el) =>
+    el.id === id && (isRadio(el) || isCheckbox(el))
+      ? { ...el, choices: [...(el.choices ?? []), { value: `opt${el.choices?.length ?? 0}` }] }
+      : el
+  );
+  pages[pageIdx] = page;
+  setSurvey((s) => ({ ...s, pages }));
+}
+
+function updateChoice(id: string, idx: number, patch: Partial<Choice>) {
+  const pages = [...survey.pages];
+  const page = { ...pages[pageIdx] };
+  page.elements = page.elements.map((el) => {
+    if (el.id !== id || !(isRadio(el) || isCheckbox(el))) return el;
+    const ch = [...(el.choices ?? [])];
+    ch[idx] = { ...ch[idx], ...patch };
+    return { ...el, choices: ch };
+  });
+  pages[pageIdx] = page;
+  setSurvey((s) => ({ ...s, pages }));
+}
+
+function removeChoice(id: string, idx: number) {
+  const pages = [...survey.pages];
+  const page = { ...pages[pageIdx] };
+  page.elements = page.elements.map((el) => {
+    if (el.id !== id || !(isRadio(el) || isCheckbox(el))) return el;
+    const ch = [...(el.choices ?? [])];
+    ch.splice(idx, 1);
+    return { ...el, choices: ch };
+  });
+  pages[pageIdx] = page;
+  setSurvey((s) => ({ ...s, pages }));
+}
+
+
+
   return (
     <div>
       
