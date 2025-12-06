7fb5afff545840f219511097a65df58c2c54d224
Koyo Hashi <126726684+koyohashi@users.noreply.github.com>
2025-12-05 20:15:50 -0500

    Update QrCode.tsx

--- app/course/[course_id]/manage/polls/[poll_id]/responses/QrCode.tsx
+++ app/course/[course_id]/manage/polls/[poll_id]/responses/QrCode.tsx
@@ -1,85 +1,72 @@
 "use client";
 
 import { useState } from "react";
-import {
-    DialogRoot,
-    DialogContent,
-    DialogBody,
-    DialogCloseTrigger
-} from "@/components/ui/dialog";
+import { DialogRoot, DialogContent, DialogBody, DialogCloseTrigger } from "@/components/ui/dialog";
 
 type QrCodeProps = {
-    qrCodeUrl: string | null;
-    size?: string;
-    isFullscreen?: boolean;
+  qrCodeUrl: string | null;
+  size?: string;
+  isFullscreen?: boolean;
 };
 
 export default function QrCode({ qrCodeUrl, size = "40px", isFullscreen = false }: QrCodeProps) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    if (!qrCodeUrl) {
-        return null;
-    }
+  if (!qrCodeUrl) {
+    return null;
+  }
 
-    return (
-        <>
-            {/* QR Code Thumbnail */}
+  return (
+    <>
+      {/* QR Code Thumbnail */}
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={qrCodeUrl}
+        alt="QR Code"
+        onClick={() => setIsModalOpen(true)}
+        style={{
+          width: size,
+          height: size,
+          flexShrink: 0,
+          display: "inline-block",
+          cursor: "pointer",
+          transition: "transform 0.2s"
+        }}
+        onMouseEnter={(e) => {
+          e.currentTarget.style.transform = "scale(1.1)";
+        }}
+        onMouseLeave={(e) => {
+          e.currentTarget.style.transform = "scale(1)";
+        }}
+      />
+
+      {/* Full Screen Modal */}
+      <DialogRoot open={isModalOpen} onOpenChange={(e) => setIsModalOpen(e.open)} closeOnInteractOutside={true}>
+        <DialogContent bg="transparent" boxShadow="none" w="fit-content" maxW="none" p={0} portalled={!isFullscreen}>
+          <DialogBody p={0} position="relative">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-                src={qrCodeUrl}
-                alt="QR Code"
-                onClick={() => setIsModalOpen(true)}
-                style={{
-                    width: size,
-                    height: size,
-                    flexShrink: 0,
-                    display: "inline-block",
-                    cursor: "pointer",
-                    transition: "transform 0.2s"
-                }}
-                onMouseEnter={(e) => {
-                    e.currentTarget.style.transform = "scale(1.1)";
-                }}
-                onMouseLeave={(e) => {
-                    e.currentTarget.style.transform = "scale(1)";
-                }}
+              src={qrCodeUrl}
+              alt="QR Code"
+              style={{
+                width: "min(75vh, 75vw)",
+                height: "min(75vh, 75vw)"
+              }}
             />
-
-            {/* Full Screen Modal */}
-            <DialogRoot open={isModalOpen} onOpenChange={(e) => setIsModalOpen(e.open)} closeOnInteractOutside={true}>
-                <DialogContent
-                    bg="transparent"
-                    boxShadow="none"
-                    w="fit-content"
-                    maxW="none"
-                    p={0}
-                    portalled={!isFullscreen}
-                >
-                    <DialogBody p={0} position="relative">
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <img
-                            src={qrCodeUrl}
-                            alt="QR Code"
-                            style={{
-                                width: "min(75vh, 75vw)",
-                                height: "min(75vh, 75vw)"
-                            }}
-                        />
-                        <DialogCloseTrigger
-                            position="absolute"
-                            top="8px"
-                            right="8px"
-                            bg="black"
-                            color="white"
-                            borderRadius="full"
-                            w="40px"
-                            h="40px"
-                            _hover={{ bg: "gray.800" }}
-                        />
-                    </DialogBody>
-                </DialogContent>
-            </DialogRoot>
-        </>
-    );
+            <DialogCloseTrigger
+              position="absolute"
+              top="8px"
+              right="8px"
+              bg="black"
+              color="white"
+              borderRadius="full"
+              w="40px"
+              h="40px"
+              _hover={{ bg: "gray.800" }}
+            />
+          </DialogBody>
+        </DialogContent>
+      </DialogRoot>
+    </>
+  );
 }
-
