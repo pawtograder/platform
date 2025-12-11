7c84c182a17e5bf038d8fb8df1d53b6cc608db0f
leozhaou <leozhaou@gmail.com>
2025-12-11 17:54:22 -0500

    Fixed the keyboard accessibility issue

--- app/course/[course_id]/manage/polls/[poll_id]/responses/QrCode.tsx
+++ app/course/[course_id]/manage/polls/[poll_id]/responses/QrCode.tsx
@@ -19,26 +19,39 @@ export default function QrCode({ qrCodeUrl, size = "40px", isFullscreen = false 
   return (
     <>
       {/* QR Code Thumbnail */}
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src={qrCodeUrl}
-        alt="QR Code"
+      <button
+        type="button"
         onClick={() => setIsModalOpen(true)}
-        style={{
-          width: size,
-          height: size,
-          flexShrink: 0,
-          display: "inline-block",
-          cursor: "pointer",
-          transition: "transform 0.2s"
-        }}
         onMouseEnter={(e) => {
           e.currentTarget.style.transform = "scale(1.1)";
         }}
         onMouseLeave={(e) => {
           e.currentTarget.style.transform = "scale(1)";
         }}
-      />
+        aria-label="Open QR code modal"
+        style={{
+          width: size,
+          height: size,
+          flexShrink: 0,
+          display: "inline-block",
+          cursor: "pointer",
+          transition: "transform 0.2s",
+          padding: 0,
+          border: "none",
+          background: "transparent"
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={qrCodeUrl}
+          alt="QR Code"
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "block"
+          }}
+        />
+      </button>
 
       {/* Full Screen Modal */}
       <DialogRoot open={isModalOpen} onOpenChange={(e) => setIsModalOpen(e.open)} closeOnInteractOutside={true}>
